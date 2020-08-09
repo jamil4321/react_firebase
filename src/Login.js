833e4430bbd7479e2081d62b8cff145effe60fe5
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2020-08-09 10:05:14 +0500

    alert

--- src/Login.js
+++ src/Login.js
@@ -7,37 +7,43 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import {withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+// import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
-const useStyles =theme => ({
+const useStyles = theme => ({
     paper: {
-      marginTop: theme.spacing(1),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+        marginTop: theme.spacing(1),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
     avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
     },
     form: {
-      width: '95%', // Fix IE 11 issue.
-      marginTop: theme.spacing(1),
+        width: '95%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
     },
     submit: {
-      margin: theme.spacing(3, 0, 2),
-      backgroundColor: '#9CB83C;',
-      borderRadius:'71px',
-      minWidth:'180px',
-      color:'white',
+        margin: theme.spacing(3, 0, 2),
+        backgroundColor: '#9CB83C;',
+        borderRadius: '71px',
+        minWidth: '180px',
+        color: 'white',
         '&:hover': {
             backgroundColor: '#617e04',
-          },
+        },
     },
-  });
-  
+});
+
 
 
 
@@ -46,9 +52,12 @@ class Login extends Component {
         super(props);
         this.login = this.login.bind(this);
         this.hanleChange = this.hanleChange.bind(this);
+        this.handleClose = this.handleClose.bind(this)
         this.state = {
             email: "",
             password: "",
+            open: false,
+            error:''
         }
     }
     login(e) {
@@ -59,7 +68,10 @@ class Login extends Component {
             localStorage.setItem("uid", u.user.uid)
         }).catch(err => {
             console.log(err)
-            window.alert(err.message)
+            this.setState({error:err.message})
+            this.setState({ open: true })
+            // window.alert(err.message)
+            
         })
 
     }
@@ -68,52 +80,68 @@ class Login extends Component {
             [e.target.name]: e.target.value
         })
     }
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        this.setState({open:false});
+      }
+
     render() {
         const { classes } = this.props;
+        // const alert = <Alert variant="outlined" severity="error" >This is a success alert — check it out!</Alert>;
         return (
             <div className="App-header">
                 <div className="App">
-                <Container component="main" maxWidth="xs" className="loginBG">
-                    <CssBaseline />
-                    <div className={classes.paper}>
-                        <Typography component="h1" variant="h3">
-                            Sing In
+                    <Snackbar open={this.state.open} autoHideDuration={5000} onClose={this.handleClose}>
+                        <Alert onClose={this.handleClose} severity="error">
+                            {/* This is a success message! */}
+                            {this.state.error}
+                        </Alert>
+                    </Snackbar>
+                    <Container component="main" maxWidth="xs" className="loginBG">
+                        <CssBaseline />
+                        <div className={classes.paper}>
+                            <Typography component="h1" variant="h3">
+                                Sing In
                         </Typography>
-                        <form className={classes.form} noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                onChange={this.hanleChange}
-                                value={this.state.email}
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="password"
-                                label="password"
-                                name="password"
-                                type="password"
-                                onChange={this.hanleChange}
-                                value={this.state.password}
-                            />
-                            <Button
-                                variant="contained"
-                                // fullWidth
-                                className={classes.submit}
-                                onClick={this.login}
-                            >
-                                Sign In
+                            <form className={classes.form} noValidate>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    onChange={this.hanleChange}
+                                    value={this.state.email}
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="password"
+                                    label="password"
+                                    name="password"
+                                    type="password"
+                                    onChange={this.hanleChange}
+                                    value={this.state.password}
+                                />
+                                <Button
+                                    variant="contained"
+                                    // fullWidth
+                                    className={classes.submit}
+                                    onClick={this.login}
+                                >
+                                    Sign In
                             </Button>
-                        </form>
-                    </div>
-                </Container>
+                                {/* {this.state.isLogedin ? null : alert} */}
+                            </form>
+                        </div>
+                    </Container>
                 </div>
             </div>
         )
@@ -122,7 +150,7 @@ class Login extends Component {
 
 Login.propTypes = {
     classes: PropTypes.object.isRequired,
-  };
+};
 
 export default withStyles(useStyles)(Login);
 
